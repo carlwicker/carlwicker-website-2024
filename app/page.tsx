4087d1b10eea051fcb41d525845f13672de818aa
Carl Wicker <carlwicker@gmail.com>
2024-06-25 15:29:02 +0100

    mobile css

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,19 @@ export default function Home() {
   return (
     <>
       <main data-scroll-section>
-        <div className="flex min-h-screen flex-col justify-center p-24 bg-black text-white uppercase">
-          <h1 className="text-9xl font-bold uppercase">...</h1>
+        <div className="flex min-h-screen flex-col justify-center lg:p-24 bg-black text-white uppercase p-5">
+          <h1 className="lg:text-9xl text-4xl font-bold uppercase">...</h1>
           <span>
             <a
               href="mailto:carlwicker@gmail.com"
-              className="hover:text-red-400 text-3xl"
+              className="hover:text-red-400 lg:text-3xl text-xl"
             >
               carlwicker@gmail.com
             </a>
           </span>
-          <span className="text-3xl">44 7810 830237</span>
+          <span className="hover:text-red-400 lg:text-3xl text-xl">
+            <a href="tel:44 7810 830237">44 7810 830237</a>
+          </span>
         </div>
 
         <CyberCortexVideo />
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import FooterEnd from "./FooterEnd";
 
 export default function Footer() {
   return (
-    <footer className="flex min-h-screen items-center justify-center bg-black text-white text-3xl uppercase">
-      <div className="container flex">
+    <footer className="flex min-h-screen items-center justify-center bg-black text-white lg:text-3xl text-xl uppercase lg:p-24 p-5">
+      <div className="container flex flex-col lg:flex-row gap-5">
         <div className="w-full">
           <FooterStart />
         </div>
