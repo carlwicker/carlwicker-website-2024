7a47f6029969ec458837bde594d1cb29cdf21f0b
Carl Wicker <carlwicker@gmail.com>
2024-07-08 12:51:21 +0100

    bg text fix

--- components/CyberCortexVideo.tsx
+++ components/CyberCortexVideo.tsx
@@ -19,22 +19,22 @@ export default function CyberCortexVideo() {
     color: "transparent",
     WebkitTextFillColor: "transparent", // For Safari
     position: "absolute",
-    font: motionValue("900 250px sans-serif"),
-    lineHeight: motionValue("0.7"),
-    fontWeight: motionValue("900"),
-    leading: motionValue("-15px"),
-    letterSpacing: motionValue("-30px"),
+    font: "900 250px sans-serif",
+    lineHeight: "0.7",
+    fontWeight: "900",
+    leading: "-15px",
+    letterSpacing: "-30px",
     overflow: "hidden",
   };
 
   return (
     <div
-      className="container mx-auto flex items-center justify-center leading-tight tracking-[-50] w-full h-full object-cover"
+      className="container mx-auto flex  justify-center  leading-tight tracking-[-50] w-full  object-cover"
       ref={canvasRef}
     >
       <motion.span
         style={textStyle} // Apply the masking style here
-        className="text-[#111] uppercase lg:text-5xl text-2xl font-extrabold mx-auto -z-50 w-screen  overflow-hidden"
+        className="text-[#111] uppercase lg:text-5xl text-2xl font-extrabold mx-auto -z-50 w-screen  overflow-hidden "
       >
         cutting-edge technologies and jaw-dropping design catapults user
         experiences into another dimension With a relentless quest for
