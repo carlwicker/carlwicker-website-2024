7ac3f6c63b2d0d323c7d99d306374d76d3819967
Carl Wicker <carlwicker@gmail.com>
2024-07-10 14:40:51 +0100

    added experimental base mask for movement tests

--- app/alpha/page.tsx
+++ app/alpha/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function AlphaPage() {
   const [imageUrl, setImageUrl] = useState("");
@@ -14,12 +14,10 @@ export default function AlphaPage() {
   useEffect(() => {
     const fetchImage = async () => {
       const accessKey = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
-      console.log(accessKey);
       const response = await fetch(
         `https://api.unsplash.com/photos/?client_id=${accessKey}`
       );
       const data = await response.json();
-      console.log(data);
       setImageUrl(data[randomNumBetween()]?.urls.regular);
     };
 
@@ -27,31 +25,48 @@ export default function AlphaPage() {
   }, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      {imageUrl !== "" && (
-        <Image
-          src={imageUrl}
-          alt="Random Unsplash Image"
-          layout="fill"
-          objectFit="cover"
-          priority={true}
-        />
-      )}
-      <Link href="/" rel="preload">
-        <button
-          className="bg-black hover:text-black hover:bg-white text-white absolute z-20 text-4xl"
-          style={{ fontFamily: "sans-serif" }}
-        >
-          HOME
-        </button>
-      </Link>
-    </div>
+    <>
+      <style jsx>{`
+        .masked-image-container {
+          position: relative;
+          width: 100%;
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          overflow: hidden;
+          perspective: 500px; /* Adds perspective to the container */
+        }
+        .text-mask {
+          font-size: 130vh;
+          font-weight: 800;
+          letter-spacing: -0.07em;
+          color: transparent;
+          background: url("${imageUrl}") center / cover no-repeat;
+          -webkit-background-clip: text;
+          background-clip: text;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+          transform: rotateY(20deg) rotateX(42deg) rotateZ(25deg) scale(1.1); /* Crazy perspective effect */
+          transform-style: preserve-3d; /* Ensures the 3D effect is rendered properly */
+        }
+      `}</style>
+      <div className="masked-image-container">
+        <div className="">
+          <div className="text-mask">UX:UI</div>
+        </div>
+      </div>
+    </>
   );
+}
+
+{
+  /* <Link h</Link>ref="/" rel="preload">
+<button className="bg-black hover:bg-white hover:text-black text-white absolute text-4xl font-sans z-20">
+  HOME
+</button>
+</Link> */
 }
