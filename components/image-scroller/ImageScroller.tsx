127cec993b4cc5a9eb466957d4da372148b99212
Carl Wicker <carlwicker@gmail.com>
2024-08-01 16:42:33 +0100

    experimental image scroll

--- components/image-scroller/ImageScroller.tsx
+++ components/image-scroller/ImageScroller.tsx
@@ -2,8 +2,8 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 export default function ImageScroller() {
-  const scrollRef = useRef<HTMLDivElement | null>(null);
-  const { scrollYProgress } = useScroll(scrollRef);
+  const scrollRef = useRef(null);
+  const { scrollYProgress } = useScroll();
 
   const x = useTransform(scrollYProgress, [0.5, 1], ["0vw", "-100vw"]);
 
@@ -14,18 +14,18 @@ export default function ImageScroller() {
   return (
     <motion.div
       ref={scrollRef}
-      className="h-[100vh] w-[200vw]  flex flex-row"
+      className="h-[200vh] w-[200vw]  flex flex-row"
       style={{ x: x }}
     >
       <motion.div
-        className="h-[100vh] bg-green-800 w-[100vw]"
+        className="h-[200vh] bg-green-800 w-[100vw]"
         style={{
           backgroundImage: "url(./webp/painting2.webp)",
           backgroundSize: "cover",
         }}
       ></motion.div>
       <motion.div
-        className="h-[100vh] bg-blue-800 w-[100vw]"
+        className="h-[200vh] bg-blue-800 w-[100vw]"
         style={{
           backgroundImage: "url(./webp/interstellar.webp)",
           backgroundSize: "cover",
