5a2f294b6c32a0d7639c0ebe367ad4f574d5c72a
Carl Wicker <carlwicker@gmail.com>
2024-08-04 19:30:48 +0100

    removed experimental image component

--- components/image-scroller/ImageScroller.tsx
+++ components/image-scroller/ImageScroller.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Link from "next/link";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function ImageScroller() {
   const scrollRef = useRef<HTMLDivElement>(null);
+  const outerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (scrollRef.current) {
@@ -16,36 +18,50 @@ export default function ImageScroller() {
         scrollTrigger: {
           trigger: scrollRef.current,
           start: "top top",
-          end: "+=100vh",
-          scrub: 1,
+          end: "top bottom",
+          scrub: 3,
+          pin: true,
+        },
+      });
+    }
+  }, []);
 
-          markers: true,
+  useEffect(() => {
+    if (outerRef.current) {
+      gsap.to(outerRef.current, {
+        scrollTrigger: {
+          // trigger: outerRef.current,
+          start: "top top",
         },
       });
     }
   }, []);
 
   return (
-    <div
-      className="h-[100vh] w-[200vw] flex flex-row overflow-y-hidden"
-      ref={scrollRef}
-    >
+    <div className="h-[50vh] w-[200vw] overflow-hidden" ref={outerRef}>
       <div
-        className="h-[100vh] bg-green-800 w-[100vw]"
-        style={{
-          backgroundImage: "url(./webp/painting2.webp)",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      ></div>
-      <div
-        className="h-[100vh] bg-blue-800 w-[100vw]"
-        style={{
-          backgroundImage: "url(./webp/interstellar.webp)",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      ></div>
+        className="h-[100vh] w-[200vw] flex flex-row overflow-y-hidden"
+        ref={scrollRef}
+      >
+        <div
+          className="h-[100vh] bg-green-800 w-[100vw]"
+          style={{
+            backgroundImage: "url(./webp/painting2.webp)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <Link href="/interstellar" passHref target="_top">
+          <div
+            className="h-[100vh] bg-blue-800 w-[100vw]"
+            style={{
+              backgroundImage: "url(./webp/interstellar.webp)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+        </Link>
+      </div>
     </div>
   );
 }
