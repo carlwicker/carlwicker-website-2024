f6ebeff7184d18469011aa46865df65a36433a14
Carl Wicker <carlwicker@gmail.com>
2024-10-19 22:22:40 +0100

    content fix

--- components/perception-typography/PerceptionTypography.tsx
+++ components/perception-typography/PerceptionTypography.tsx
@@ -13,13 +13,13 @@ export default function PerceptionTypography() {
         </ClipText> */}
         <div className="xl:w-1/2" data-scroll data-scroll-speed="0.05">
           <LineSplit>
-            I'm a web developer focused on crafting cutting-edge, dynamic user
-            experiences with Next.js, React, and TypeScript. I specialize in
+            Web developer focused on crafting cutting-edge, dynamic user
+            experiences with NextJS, React, and TypeScript. I specialize in
             high-performance interfaces, leveraging GSAP, Framer Motion, and
-            Three.js for immersive animations and interactions. With TailwindCSS
-            and a strong command of JavaScript, I create sleek, responsive, and
-            scalable applications that push the boundaries of modern web
-            development.
+            ThreeJS for immersive animations and interactions. With decades of
+            experience with CSS and a strong command of JavaScript, I create
+            sleek, responsive, and scalable applications that push the
+            boundaries of modern web development.
           </LineSplit>
         </div>
       </div>
@@ -60,13 +60,13 @@ export default function PerceptionTypography() {
         <div className="lg:text-[3rem] text-[1.4rem]">
           <div className="" data-scroll data-scroll-speed="0.05">
             <LineSplit>
-              Whether it's enhancing user experience through structured layouts
-              or elevating brand identity with powerful typography, my design
-              approach balances form and function. Each website I create is
-              built to not only look great but also provide an effortless and
-              engaging experience for the user. By focusing on detail and
-              clarity, I help clients make a lasting impression through every
-              digital touchpoint.
+              Enhancing user experience through structured layouts and elevating
+              brand identity with powerful typography, my design approach
+              balances form and function. Each website I create is built to not
+              only look great but also provide an effortless and engaging
+              experience for the user. By focusing on detail and clarity, I help
+              clients make a lasting impression through every digital
+              touchpoint.
             </LineSplit>
           </div>
         </div>
